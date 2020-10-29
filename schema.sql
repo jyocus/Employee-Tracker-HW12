c3cee6644f296069195cb2211781b22496c17b7c
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-28 23:06:31 -0500

    created tables in schema to reflect requirements in README

--- schema.sql
+++ schema.sql
@@ -3,3 +3,29 @@ CREATE DATABASE employee_DB;
 
 /* designating which database to use*/
 USE employee_DB;
+
+CREATE TABLE Department (
+    id INT NOT NULL,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE emp_role(
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+
+
