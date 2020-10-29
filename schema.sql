b4f558f250cd467fc386919c30bd7ba80af42367
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-28 23:10:22 -0500

    forgot to add auto increment to id in tables

--- schema.sql
+++ schema.sql
@@ -5,13 +5,13 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE Department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE emp_role(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE emp_role(
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
