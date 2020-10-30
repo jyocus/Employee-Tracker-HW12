80826d5988a2e3a10579caea5284be7f7c9af146
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-29 20:12:46 -0500

    update employee function added with tutor

--- tracker.js
+++ tracker.js
@@ -10,168 +10,168 @@ var connection = mysql.createConnection({
     user: "root",
     password: "password",
     database: "employee_DB"
-  });
-  
-  // connect to the mysql server and sql database
-  connection.connect(function(err) {
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     // run the start function after the connection is made to prompt the user
     start();
-  });
+});
 
-function start(){
- inquirer.prompt({
-     name: 'startQuestions',
-     message: 'What would you like to do?',
-     type: 'list',
-     choices: ['Add Department', 'Add Role', 'Add Employee', 'View Department', 'View Role', 'View Employee', 'Update Employee Roles', 'End Program']
-     
- }).then (answer =>{
-    //  console.log(response.startQuestions);
-    // Instead of writing a bunch of if/else using a switch statement to lead the user to a different function based on what they want to do
-    switch (answer.startQuestions) {
-        case 'Add Department':
-            addDepartment()
-            break;
-        case 'Add Role':
-            addRole()
-            break;
-        case 'Add Employee':
-            addEmployee()
-            break;
-        case 'View Department':
-            viewDepartment()
-            break;
-        case 'View Role':
-            viewRole()
-            break;
-        case 'View Employee':
-            viewEmployee()
-            break;
-        case 'Update Employee Roles':
-            updateEmpRoles()
-            break;
-        case 'End Program':
-            endProgram()
-            break;
+function start() {
+    inquirer.prompt({
+        name: 'startQuestions',
+        message: 'What would you like to do?',
+        type: 'list',
+        choices: ['Add Department', 'Add Role', 'Add Employee', 'View Department', 'View Role', 'View Employee', 'Update Employee Roles', 'End Program']
 
-    }
- })
+    }).then(answer => {
+        //  console.log(response.startQuestions);
+        // Instead of writing a bunch of if/else using a switch statement to lead the user to a different function based on what they want to do
+        switch (answer.startQuestions) {
+            case 'Add Department':
+                addDepartment()
+                break;
+            case 'Add Role':
+                addRole()
+                break;
+            case 'Add Employee':
+                addEmployee()
+                break;
+            case 'View Department':
+                viewDepartment()
+                break;
+            case 'View Role':
+                viewRole()
+                break;
+            case 'View Employee':
+                viewEmployee()
+                break;
+            case 'Update Employee Roles':
+                updateEmpRoles()
+                break;
+            case 'End Program':
+                endProgram()
+                break;
+
+        }
+    })
 }
 
-function addDepartment(){
+function addDepartment() {
     // console.log('working');
-inquirer.prompt({
-    name: 'addDepartment',
-    message:'What is the department you would like to add?',
-    type: 'input'
+    inquirer.prompt({
+        name: 'addDepartment',
+        message: 'What is the department you would like to add?',
+        type: 'input'
 
-//.then promise and mysql to add new department from user input to table
-}).then(answer => {
-    connection.query("INSERT INTO Department SET ?",
-    {
-        dept_name: answer.addDepartment
-    },
-    function(err, res){
-        console.log(res);
-        start();
-    }
-    );
-})
+        //.then promise and mysql to add new department from user input to table
+    }).then(answer => {
+        connection.query("INSERT INTO Department SET ?",
+            {
+                dept_name: answer.addDepartment
+            },
+            function (err, res) {
+                console.log(res);
+                start();
+            }
+        );
+    })
 }
 //Receiving Parse Error
-function addRole(){
+function addRole() {
     // console.log('working');
-inquirer.prompt([
-    {
-    name: 'roleTitle',
-    message:'What is the role you would like to add?',
-    type: 'input'
-    },
-    {
-    name: 'roleSalary',
-    message: 'What is the salary for this role?',
-    type: 'number'
-    },
-    {
-    name: 'deptID',
-    message: 'What is the department ID?',
-    type: 'number'
-    }
-//.then promise and mysql to add new role from user input to table
-]).then(answer => {
-    connection.query("INSERT INTO emp_role SET ?",
-    {
-        title: answer.roleTitle,
-        salary: answer.roleSalary,
-        department_id: answer.deptID
-    },
-    function(err, res){
-        console.log(res);
-        start();
-    }
-    );
+    inquirer.prompt([
+        {
+            name: 'roleTitle',
+            message: 'What is the role you would like to add?',
+            type: 'input'
+        },
+        {
+            name: 'roleSalary',
+            message: 'What is the salary for this role?',
+            type: 'number'
+        },
+        {
+            name: 'deptID',
+            message: 'What is the department ID?',
+            type: 'number'
+        }
+        //.then promise and mysql to add new role from user input to table
+    ]).then(answer => {
+        connection.query("INSERT INTO emp_role SET ?",
+            {
+                title: answer.roleTitle,
+                salary: answer.roleSalary,
+                department_id: answer.deptID
+            },
+            function (err, res) {
+                console.log(res);
+                start();
+            }
+        );
 
-})
+    })
 }
 //Receiving Parse Error
-function addEmployee(){
+function addEmployee() {
     // console.log('working');
-inquirer.prompt([
-    {
-    name: 'firstName',
-    message:'What is the first name of the employee?',
-    type: 'input'
-    },
-    {
-    name: 'lastName',
-    message: 'What is the last name of the employee?',
-    type: 'input'
-    },
-    {
-    name: 'roleID',
-    message: 'What is the role ID?',
-    type: 'number'
-    },
-    {
-    name:'managerID',
-    message:'What is the Manager ID (you can leave blank if N/A)',
-    type:'number'
-    }
-//.then promise and mysql to add new employee from user input to table
-]).then(answer => {
-    connection.query("INSERT INTO employee SET ?",
-    {
-        first_name: answer.firstName,
-        last_name: answer.lastName,
-        role_id: answer.roleID,
-        manager_id: answer.managerID
-    },
-    function(err, res){
-        console.log(res);
-        start();
-    }
-    );
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            message: 'What is the first name of the employee?',
+            type: 'input'
+        },
+        {
+            name: 'lastName',
+            message: 'What is the last name of the employee?',
+            type: 'input'
+        },
+        {
+            name: 'roleID',
+            message: 'What is the role ID?',
+            type: 'number'
+        },
+        {
+            name: 'managerID',
+            message: 'What is the Manager ID (you can leave blank if N/A)',
+            type: 'number'
+        }
+        //.then promise and mysql to add new employee from user input to table
+    ]).then(answer => {
+        connection.query("INSERT INTO employee SET ?",
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.roleID,
+                manager_id: answer.managerID
+            },
+            function (err, res) {
+                console.log(res);
+                start();
+            }
+        );
 
-})
+    })
 }
 
-function viewDepartment(){
-    connection.query("SELECT * FROM Department", function(err,res){
+function viewDepartment() {
+    connection.query("SELECT * FROM Department", function (err, res) {
         console.table(res);
         start();
     })
 }
 
-function viewRole(){
-    connection.query("SELECT * FROM emp_role", function(err,res){
+function viewRole() {
+    connection.query("SELECT * FROM emp_role", function (err, res) {
         console.table(res);
         start();
     })
 }
 //to view all employees requires a join - Worked on this with tutor
-function viewEmployee(){
+function viewEmployee() {
     let query = `SELECT  employee.id, employee.first_name, employee.last_name
     , emp_role.title, emp_role.salary
     , Department.dept_name
@@ -180,8 +180,8 @@ function viewEmployee(){
        ON (employee.role_id = emp_role.id) 
        LEFT JOIN Department 
        ON (emp_role.department_id = Department.id)`
-    connection.query(query, function(err,res){
-        if (err){
+    connection.query(query, function (err, res) {
+        if (err) {
             console.log(err);
         }
         console.table(res);
@@ -189,13 +189,42 @@ function viewEmployee(){
     })
 }
 
-// function updateEmpRoles(){
+function updateEmpRoles() {
+    inquirer.prompt([
+        {
+            name: 'whatEmployeeID',
+            message: 'What is the id of the employee you want to change?',
+            type: 'input'
+        },
+        {
+            name: 'whatRole',
+            message: 'What is the new role you want to assign them?',
+            type: 'input'
+        },
+        {
+            name: 'newRoleID',
+            message: 'What is the new role ID?',
+            type: 'number'
+        },
+    ]).then(answer => {
+        connection.query("UPDATE employee SET ? WHERE ?", [
+            {
 
-// }
+                role_id: answer.newRoleID
+            }, {
+                id: answer.whatEmployeeID
+            }],
+            function (err, res) {
+                console.log(res);
+                start();
+            }
+        );
+        })
+    }
 
-function endProgram(){
-    connection.end();
-}
+function endProgram() {
+            connection.end();
+        }
 
 
 
